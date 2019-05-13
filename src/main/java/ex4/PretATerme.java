aebaa8f3f118cd8966b199a6590c0a65053c8a90
Kévin SEGUINEAU <kevinseguineau+git@gmail.com>
2019-05-13 09:47:41 +0200

    ex4

--- src/main/java/ex4/PretATerme.java
+++ src/main/java/ex4/PretATerme.java
@@ -1,5 +1,11 @@
 package ex4;
 
+/**
+ * cette classe represente un pret à terme
+ * 
+ * @author Kevin.s
+ *
+ */
 public class PretATerme implements Strategy {
 
 }
--- src/main/java/ex4/Strategy.java
+++ src/main/java/ex4/Strategy.java
@@ -1,5 +1,11 @@
 package ex4;
 
+/**
+ * interface representant une strategie de pret
+ * 
+ * @author Kevin.s
+ *
+ */
 public interface Strategy {
 
 }
